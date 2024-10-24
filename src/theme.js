9eade96428bc2d55a42e45b49671ae47a6889f50
Dun <dung8ahxh@gmail.com>
2024-10-24 10:03:13 +0700

    theme config

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+// Create a theme instance.
+const theme = createTheme({
+  cssVariables: true,
+
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+    text: {
+      primary: "#333",
+      secondary: red.A400,
+    },
+  },
+});
+
+export default theme;
