c2feb6ca21326ca9ad879eb8fd5b42a62454eb91
Dun <dung8ahxh@gmail.com>
2024-10-25 10:49:52 +0700

    layout v1

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,26 @@
-import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import { cyan, deepOrange, orange, teal } from "@mui/material/colors";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 
 // Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#556cd6",
+const theme = extendTheme({
+  trelloCustome: {
+    appBarHeight: "48px",
+    boardBarHeight: "58px",
+  },
+  colorSchemes: {
+    light: {
+      // palette for light mode
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      },
     },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-    text: {
-      primary: "#333",
-      secondary: red.A400,
+    dark: {
+      // palette for dark mode
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
     },
   },
 });
